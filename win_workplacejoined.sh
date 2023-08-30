1fa3c4152c02c9ec0512bd2f27c6dbf504656ef0
fonwiz <138896225+fonwiz@users.noreply.github.com>
2023-08-30 15:05:21 +0200

    Create win_workplacejoined.sh
    inital release for workplacejoined script to identify registered devices

---
+++ win_workplacejoined.sh
@@ -0,0 +1,9 @@
+# Run as System and Automatic architecture
+# Run dsregcmd /status in a command prompt and capture the output
+$dsregcmdOutput = cmd /c "dsregcmd /status"
+
+# Parse the output for the DomainJoined key
+$workplaceJoined = ($dsregcmdOutput | Select-String 'WorkplaceJoined\s*:\s*(.*)').Matches.Groups[1].Value
+
+# Output the values of the DomainJoined key
+echo $workplaceJoined
